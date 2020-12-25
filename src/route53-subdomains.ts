cd13a02941ccbdef7533882cf0a0918f1e6611b8
Roman Naumenko <roman+git@naumenko.ca>
2020-12-25 18:30:30 -0500

    undefined check

--- src/route53-subdomains.ts
+++ src/route53-subdomains.ts
@@ -1,6 +1,6 @@
 
 import { PublicHostedZone } from '@aws-cdk/aws-route53';
-import { Stack, Construct, StackProps, CfnOutput } from '@aws-cdk/core';
+import { Stack, Construct, Fn, StackProps, CfnOutput } from '@aws-cdk/core';
  
 export class stackSettings {
   readonly stacksettings?: {
@@ -15,15 +15,21 @@ export class Route53Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps, stackconfig?: stackSettings) {
     super(scope, id, props);
     
-    const zone = new PublicHostedZone(this, stackconfig?.stacksettings?.hostedZone!, {
+    const zone = new PublicHostedZone(this, 'zone', {
       zoneName: stackconfig?.stacksettings?.environment!,
       comment: 'Hosted zone for ' + stackconfig?.stacksettings?.environment!
     });
 
-    this.ZoneInfo = new CfnOutput(this, 'ZoneNameServers', {
-      value: JSON.stringify(zone.hostedZoneNameServers?.join(",")),
+    let cfo = new CfnOutput(this, 'ZoneNameServers', {
+      value: 'UNDEFINED',
       description: 'Delegation record for zone: ' + stackconfig?.stacksettings?.hostedZone!
     });
+
+    if (zone.hostedZoneNameServers) {
+      cfo.value = Fn.join(',', zone.hostedZoneNameServers)
+      this.ZoneInfo = cfo
+    } else {
+      this.ZoneInfo = cfo
+    }
   }
 }
-
